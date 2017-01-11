a69f9e01afd3e61203e7582698f11b5ddd26c655
Shawn Sercombe <shawnsercombe@gmail.com>
2017-01-10 19:39:38 -0500

    Fixed player jump issues.
    The player should no longer be able to jump when hanging on the ledge by
    his head. Changed player's polygon collider shape. Added separate
    triggers for left and right foot.

---
+++ Assets/Scripts/RightFootCollision.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightFootCollision : MonoBehaviour {
+
+	public Player player;
+	public LeftFootCollision leftFoot;
+	public bool isGrounded;
+	private int index;
+
+	void Start(){
+		player = GameObject.Find("Player").GetComponent<Player>();
+		index = transform.GetSiblingIndex();
+		leftFoot = transform.parent.GetChild(index - 1).gameObject.GetComponent<LeftFootCollision>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Platform"){
+			isGrounded = true;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		if (other.gameObject.tag == "Platform"){
+			if (leftFoot.isGrounded){
+				SetGrounded();
+			}
+			else if (player.rb2d.velocity.y == 0){
+				SetGrounded();
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.gameObject.tag == "Platform"){
+			isGrounded = false;
+		}
+	}
+
+	void SetGrounded(){
+		player.GetComponent<Animator>().SetBool("isJumping", false);
+		player.GetComponent<Animator>().SetBool("isFalling", false);
+		player.isGrounded = true;
+		player.isJumping = false;
+		player.hasDoubleJumped = false;
+	}
+}
