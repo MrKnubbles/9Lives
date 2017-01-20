606ea1f5eb1ba8303184efed3b4cfd2a32bd6571
Shawn Sercombe <shawnsercombe@gmail.com>
2017-01-20 01:21:02 -0500

    Added level 33 with ElectricFence.
    Created level 33 and updated the old ElectricFence object.

--- Assets/Scripts/Traps/ElectricFence.cs
+++ Assets/Scripts/Traps/ElectricFence.cs
@@ -6,6 +6,11 @@ public class ElectricFence : MonoBehaviour {
 	public ScoreTracker time;
 	public GameObject electricity;
 
+	void Start(){
+		time = GameObject.Find("HUD").GetComponent<ScoreTracker>();
+		electricity = transform.GetChild(0).gameObject;
+	}
+
 	void Update(){
 		if (time.GetTime() % 3 == 0){
 			electricity.SetActive(true);
