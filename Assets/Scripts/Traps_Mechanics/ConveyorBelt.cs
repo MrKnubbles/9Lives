9da29729c4b0f4c0557e8f05652de2b6517e6981
Shawn Sercombe <shawnsercombe@gmail.com>
2017-02-24 22:58:06 -0500

    Merged in World2 traps and updated art on them.
    Updated art on World2 traps. Created LaunchPad animation.

---
+++ Assets/Scripts/Traps_Mechanics/ConveyorBelt.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour {
+
+	public bool m_onConveyor;
+	private GameObject m_player;
+
+	void Start() {
+		m_onConveyor = false;
+		m_player = GameObject.Find("Player");
+	}
+
+	void Update() {
+		if(m_onConveyor) {
+			m_player.transform.position += new Vector3(2f, 0, 0) * Time.deltaTime;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		if(other.CompareTag("Player")) {
+			m_onConveyor = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.CompareTag("Player")) {
+			m_onConveyor = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Traps_Mechanics/SpikePress.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikePress : MonoBehaviour {
+
+	public GameObject m_leftSpike;
+	public GameObject m_rightSpike;
+	private Vector2 m_leftSpikeStartLocation;
+	private Vector2 m_rightSpikeStartLocation;
+	private Vector2 m_middlePosition;
+	public bool m_insideTrap;
+	private float m_closeSpeed = 0.02f;
+	private float m_openSpeed = 0.1f;
+
+	void Start() {
+		m_leftSpikeStartLocation = m_leftSpike.transform.position;
+		m_rightSpikeStartLocation = m_rightSpike.transform.position;
+	}
+
+	void Update() {
+		if(m_insideTrap) {
+			CloseSpikes();
+		} else {
+			OpenSpikes();
+		}
+	}
+	
+	void OnTriggerStay2D(Collider2D other) {
+		if(other.CompareTag("Player")) {
+			m_insideTrap = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.CompareTag("Player")) {
+			m_insideTrap = false;
+		}
+	}
+
+	void CloseSpikes() {
+		m_middlePosition = (m_rightSpike.transform.position / 2) + (m_leftSpike.transform.position / 2);
+		m_leftSpike.transform.position = Vector2.MoveTowards(m_leftSpike.transform.position, m_middlePosition, m_closeSpeed);
+		m_rightSpike.transform.position = Vector2.MoveTowards(m_rightSpike.transform.position, m_middlePosition, m_closeSpeed);
+	}
+
+	void OpenSpikes() {
+		m_leftSpike.transform.position = Vector2.MoveTowards(m_leftSpike.transform.position, m_leftSpikeStartLocation, m_openSpeed);
+		m_rightSpike.transform.position = Vector2.MoveTowards(m_rightSpike.transform.position, m_rightSpikeStartLocation, m_openSpeed);
+	}
+}
