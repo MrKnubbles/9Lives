f4619e2c3d7fb1d60bac7d0552a72b23012cec05
YorBoyBlue <arin.a.h.blue@gmail.com>
2017-11-18 11:54:23 -0500

    Level 13 Tut done

---
+++ Assets/Scripts/TutorialStuff/World1/Level13Tut.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level13Tut : MonoBehaviour {
+
+	Vector3 m_messagePosition = Vector3.zero;
+
+	void Start () {
+		if(!PlayerPrefs.HasKey("Level13TutStartTriggered")) {
+			HintSystemCanvas.m_singleton.DisplayDefaultHint("Looks like some of those spikes on the roof are loose. Watch out for falling traps!", m_messagePosition);
+			PlayerPrefs.SetString("Level13TutStartTriggered", "");
+		}
+	}
+}
