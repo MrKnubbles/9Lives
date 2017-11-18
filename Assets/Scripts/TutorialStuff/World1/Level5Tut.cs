23b858ab6ae6c98c3e4657f4c7a2f254e9a7a54e
YorBoyBlue <arin.a.h.blue@gmail.com>
2017-11-18 11:48:41 -0500

    Level 5 tut done

---
+++ Assets/Scripts/TutorialStuff/World1/Level5Tut.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level5Tut : MonoBehaviour {
+
+	Vector3 m_messagePosition = Vector3.zero;
+
+	void Start () {
+		if(!PlayerPrefs.HasKey("Level5TutStartTriggered")) {
+			HintSystemCanvas.m_singleton.DisplayDefaultHint("Looks like there is a new trap! Watch out for those saws, they look dangerous!", m_messagePosition);
+			PlayerPrefs.SetString("Level5TutStartTriggered", "");
+		}
+	}
+}
