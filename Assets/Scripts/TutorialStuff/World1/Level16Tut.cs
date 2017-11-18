89021b45beea21dadaae4aa9306ac6265f8327fc
YorBoyBlue <arin.a.h.blue@gmail.com>
2017-11-18 11:57:15 -0500

    Level 16 Tut done

---
+++ Assets/Scripts/TutorialStuff/World1/Level16Tut.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level16Tut : MonoBehaviour {
+
+	Vector3 m_messagePosition = Vector3.zero;
+
+	void Start () {
+		if(!PlayerPrefs.HasKey("Level16TutStartTriggered")) {
+			HintSystemCanvas.m_singleton.DisplayDefaultHint("Don't fall into that green goo! It doesn't look safe.", m_messagePosition);
+			PlayerPrefs.SetString("Level16TutStartTriggered", "");
+		}
+	}
+}
